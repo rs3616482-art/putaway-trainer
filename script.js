2df8a60648d7405fbda012b061018d6cf2fa6afd
rs3616482-art <rs3616482@gmail.com>
2026-07-27 01:01:36 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PutAway Trainer</title>
+<link rel="stylesheet" href="style.css">
+</head>
+<body>
+
+<header>
+    <h2>Put Away</h2>
+</header>
+
+<div class="container">
+    <div class="icone">📦</div>
+    <h1>Escaneie a Tote</h1>
+    <input id="scanner" type="text" autofocus placeholder="Aguardando leitura...">
+</div>
+
+<script src="script.js"></script>
+
+</body>
+</html>
